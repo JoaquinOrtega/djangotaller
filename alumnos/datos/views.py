985c3b36a61c15cc2f547988fd4874e08db51ffd
joaquin Comes Here <Joaco-8@outlook.com>
2016-11-11 14:07:03 -0600

    Agregando archivos con forms

--- alumnos/datos/views.py
+++ alumnos/datos/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import InfoGeneralForm
 
 def hola(request):
     return render(request, "index.html", {"nombre": "Joaquin Ortega"})
 
 def formulario(request):
-    form = "Hola este es mi formulario"
+    form = InfoGeneralForm()
     return render(request, "formulario.html", {"formulario": form})
 # Create your views here.
